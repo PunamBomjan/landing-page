ee0c5afeb4c2ce53ba4a8731234fddf3a5dd279c
PunamBomjan <punambomjan3@gmail.com>
2024-07-27 19:48:41 +0545

    my first commit

---
+++ L_Page-3/script.js
@@ -0,0 +1,23 @@
+gsap.from(".img-1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from(".img-2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from(".img-3",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
+gsap.from(".main h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
